19caeda677e704fd4bf325a25f352d1396d1a1f8
jpwiig <jpwiig@gmail.com>
2023-04-05 21:17:17 +0200

    the bot works, time to add functions

--- bot.py
+++ bot.py
@@ -1,47 +1,51 @@
-#creation of a discord bot to learn more python
+# creation of a discord bot to learn more python
 import discord
-from  discord.ext import commands
+from discord.ext import commands
 import os
 import response
 from dotenv import load_dotenv
 import logging
+
 load_dotenv()
 
 logger = logging.getLogger('Discord')
 
-async def send_message(message, user_message, is_private): 
-    try: 
-        response = response.responses(user_message)
-        await message.author.send(response) if is_private else await message.channel.send()
-    except Exception as e: 
-        print(e) 
-#starts the discord bot
-#bot.run(discord_token)
+
+async def send_message(message, user_message, is_private):
+    try:
+        ans = response.respones(user_message)
+        await message.author.send(ans) if is_private else await message.channel.send(ans)
+    except Exception as e:
+        print(f'error inside Exception {e}')
+    # starts the discord bot
+
+
+# bot.run(discord_token)
 
 def start_bot():
-    #client = discord.Client()
+    # client = discord.Client()
     discord_token = os.getenv('API_KEY')
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
-    @client.event
-    async def on_ready(): 
-       print('shite ready')
 
     @client.event
-    async def on_message(msg): 
+    async def on_ready():
+        print('shite ready')
+
+    @client.event
+    async def on_message(msg):
         if msg.author == client.user:
             return
-    
+
         username = str(msg.author)
         channel = str(msg.channel)
         user_content = str(msg.content)
-        print (f'{username} said :  "{user_content}" in {channel}')
-        responses(user_content)
-        if user_content[0] == '?': 
-         user_content = user_content [1:]
-         await send_message(msg, user_content,is_private=True)
-        else: 
+        print(f'{username} said :  "{user_content}" in {channel}')
+        if user_content[0] == '?':
+            user_content = user_content[1:]
+            await send_message(msg, user_content, is_private=True)
+        else:
             await send_message(msg, user_content, is_private=False)
 
     client.run(discord_token)
--- response.py
+++ response.py
@@ -1,14 +1,14 @@
 def respones(mssge : str) -> str: 
-    message = mssge.lower()
+    message = mssge
 
-    if(message == 'mus'):
+    if message == 'mus':
         return 'ehus'
-    if(message == 'rick'):
+    if message == 'rick':
         return 'never gonna give you up, never gonna let you down'
     
-    if(message == 'https://tenor.com/view/ruben-vareide-ruben-vareide-prebz-og-dennis-prebz-gif-26529878'):
+    if message == 'https://tenor.com/view/ruben-vareide-ruben-vareide-prebz-og-dennis-prebz-gif-26529878':
         return 'https://media1.tenor.com/images/c5d7235861884672feac3bc93f20d5da/tenor.gif?itemid=26839897'    
-    if(message =='!hva'):
+    if message =='!hva':
         return 'hjelpemelding din nørd'
     
     return 'hei du jeg syns du er en tulling'
