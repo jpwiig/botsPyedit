0d0bb65e4d771919622ad5b49c56f11301537da5
jpwiig <jpwiig@gmail.com>
2023-04-18 10:44:24 +0200

    fixing

--- response.py
+++ response.py
@@ -1,16 +1,16 @@
-def respones(mssge : str) -> str: 
+def respones(mssge: str) -> str:
     message = mssge.lower()
 
     if message == 'mus':
         return 'ehus'
-    if message == 'rick':
+    elif message == 'rick':
         return 'never gonna give you up, never gonna let you down'
-    
-    if message == 'https://tenor.com/view/ruben-vareide-ruben-vareide-prebz-og-dennis-prebz-gif-26529878':
-        return 'https://media1.tenor.com/images/c5d7235861884672feac3bc93f20d5da/tenor.gif?itemid=26839897'    
-    if message =='!hva':
+    elif message == 'https://tenor.com/view/ruben-vareide-ruben-vareide-prebz-og-dennis-prebz-gif-26529878':
+        return 'https://media1.tenor.com/images/c5d7235861884672feac3bc93f20d5da/tenor.gif?itemid=26839897'
+    elif message == '!hva':
         return 'hjelpemelding din nørd'
-    if message == 'faen' or 'fuck deg':
+    if message == 'faen' or message == 'fuck deg':
         return 'ikke bann!'
-
-    return 'tulling'
+    else:
+        return None
+# return 'tulling'
